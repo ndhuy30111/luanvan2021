11d4a6524f5d7e6f1153fed6113bc02fa5467e67
ndhuy30111 <dh51701452@student.stu.edu.vn>
2021-05-02 20:43:28 +0700

    v0.0.4

--- backend/app/Models/ProductsDiscount.php
+++ backend/app/Models/ProductsDiscount.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class products_discount extends Model
+class ProductsDiscount extends Model
 {
     use HasFactory;
 }
