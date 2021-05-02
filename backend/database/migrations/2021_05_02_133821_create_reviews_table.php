0ea6641b74ab131a06fa11bb8362c0264643dbb2
ndhuy30111 <dh51701452@student.stu.edu.vn>
2021-05-02 20:40:41 +0700

    migrations

--- backend/database/migrations/2021_05_02_133821_create_reviews_table.php
+++ backend/database/migrations/2021_05_02_133821_create_reviews_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRatesTable extends Migration
+class CreateReviewsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class CreateRatesTable extends Migration
      */
     public function up()
     {
-        Schema::create('rates', function (Blueprint $table) {
+        Schema::create('reviews', function (Blueprint $table) {
             $table->integer('user_id');
             $table->integer('product_id');
             $table->tinyInteger('rate');
+            $table->text('comment');
             $table->primary(['user_id', 'product_id']);
             $table->foreign('user_id')->references('id')->on('users');
             $table->foreign('product_id')->references('id')->on('products');
-
         });
     }
 
@@ -31,6 +31,6 @@ class CreateRatesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('rates');
+        Schema::dropIfExists('reviews');
     }
 }
