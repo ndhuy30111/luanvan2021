83c86b4f721be19ced941666cd3a8dfdaaac5afe
MyDuyen0612 <dh51704984@student.stu.edu.vn>
2021-06-06 17:42:23 +0700

    commit vuex categorys

--- frontend/store/user/categorys/mutations.js
+++ frontend/store/user/categorys/mutations.js
@@ -1,12 +1,12 @@
 export default {
-  ACTIONS_CATEGORYS_GETALL(state, payload) {
+  MUTATIONS_CATEGORYS_GETALL(state, payload) {
     state.categorys = payload.categorys
     state.select = payload.select
   },
 
-  removeProductCart(state, indexremove){
-    state.cart = state.cart.filter((item, index) =>{
-      return index !== indexremove;
+  removeProductCart(state, indexremove) {
+    state.cart = state.cart.filter((item, index) => {
+      return index !== indexremove
     })
   },
 }
