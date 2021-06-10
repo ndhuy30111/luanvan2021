4125b2f9096caf849ec6aa3f11530fd591e58cde
ndhuy30111 <dh51701452@student.stu.edu.vn>
2021-06-10 22:12:51 +0700

    Update

---
+++ frontend/locales/admin/headerTable.js
@@ -0,0 +1,42 @@
+export default () => ({
+  CATEGORY_HEADER: [
+    {
+      text: 'Tên danh mục',
+      align: 'start',
+      value: 'name',
+    },
+    { text: 'Đường dẫn', value: 'url' },
+    { text: 'Độ ưu tiên', value: 'sort' },
+    { text: 'Cha', value: 'category' },
+    { text: 'Ngày tạo', value: 'createDate' },
+    { text: 'Người tạo', value: 'createBy' },
+    { text: 'Người cập nhập', value: 'lastModifiedBy' },
+    { text: 'Ngày cập nhập', value: 'lastModifiedDate' },
+
+    { text: '', value: 'actions', sortable: false },
+  ],
+  COLOR_HEADER: [
+    {
+      text: 'Tên màu',
+      align: 'start',
+      value: 'name',
+    },
+    { text: 'mã màu', value: 'code' },
+    { text: '', value: 'actions', sortable: false },
+  ],
+  PRODUCT_HEADER: [
+    {
+      text: 'Tên danh mục',
+      align: 'start',
+      value: 'name',
+    },
+    { text: 'Đường dẫn', value: 'url' },
+    { text: 'Danh mục', value: 'category' },
+    { text: 'Ngày tạo', value: 'createDate' },
+    { text: 'Người tạo', value: 'createBy' },
+    { text: 'Người cập nhập', value: 'lastModifiedBy' },
+    { text: 'Ngày cập nhập', value: 'lastModifiedDate' },
+
+    { text: '', value: 'actions', sortable: false },
+  ],
+})
