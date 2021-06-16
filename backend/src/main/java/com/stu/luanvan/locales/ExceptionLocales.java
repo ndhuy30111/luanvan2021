7915b39053a840092fa479021a85081f9245dee6
MyDuyen0612 <dh51704984@student.stu.edu.vn>
2021-06-16 23:06:44 +0700

    commit update mes

--- backend/src/main/java/com/stu/luanvan/locales/ExceptionLocales.java
+++ backend/src/main/java/com/stu/luanvan/locales/ExceptionLocales.java
@@ -10,4 +10,5 @@ public class ExceptionLocales {
     public final static String RequestMethodNotSupport="No support method";
     public final static String FOUND_ERROR= "You cannot access";
     public final static String NAME_SAKE= "Namesake";
+    public final static String EMAIL_SAKE= "Emailsake";
 }
